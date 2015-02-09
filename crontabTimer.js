aa85204ce95648394f7a473de6890c8a8b35db64
Widyou <hi@widyou.net>
2015-02-09 18:12:55 +0900

    change methodsForTimeUnit. add Object.keys for old IE.

--- crontabTimer.js
+++ crontabTimer.js
@@ -1,51 +1,96 @@
 "use strict";
+
+// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+if (!Object.keys) {
+	Object.keys = (function () {
+		var hasOwnProperty = Object.prototype.hasOwnProperty,
+			hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+			dontEnums = [
+				'toString',
+				'toLocaleString',
+				'valueOf',
+				'hasOwnProperty',
+				'isPrototypeOf',
+				'propertyIsEnumerable',
+				'constructor'
+			],
+			dontEnumsLength = dontEnums.length;
+		return function (obj) {
+			if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+				throw new TypeError('Object.keys called on non-object');
+			}
+			var result = [], prop, i;
+			for (prop in obj) {
+				if (hasOwnProperty.call(obj, prop)) {
+					result.push(prop);
+				}
+			}
+			if (hasDontEnumBug) {
+				for (i = 0; i < dontEnumsLength; i++) {
+					if (hasOwnProperty.call(obj, dontEnums[i])) {
+						result.push(dontEnums[i]);
+					}
+				}
+			}
+			return result;
+		};
+	}());
+}
+
+// define base methods for each unit
 var methodsForTimeUnit = {
 	minute: {
-		get: function () {
-			return this.getMinutes();
-		},
-		add: function (amount) {
-			this.setMinutes(this.getMinutes() + amount);
-		},
-		next: function (target) {
-			var thisMin = this.getMinutes();
-			if (thisMin <= target) {
-				this.setMinutes(this.getMinutes() + (target - thisMin));
-			} else {
-				this.setMinutes(this.getMinutes() + (target + 60 - thisMin));
-			}
-		}
+		get: Date.prototype.getMinutes,
+		set: Date.prototype.setMinutes
 	},
 	hour: {
-		get: function () {
-			return this.getHours();
-		}
+		get: Date.prototype.getHours,
+		set: Date.prototype.setHours
 	},
 	dayOfMonth: {
-		get: function () {
-			return this.getDate();
-		}
+		get: Date.prototype.getDate,
+		set: Date.prototype.setDate
 	},
 	month: {
-		get: function () {
-			return this.getMonth() + 1;
-		}
+		get: function () { return this.getMonth() + 1; },
+		set: function (month) { return this.setMonth(month - 1); }
 	},
 	DayOfWeek: {
-		get: function () {
-			return this.getDay();
+		get: Date.prototype.getDay,
+		set: function (dow) {
+			var thisDow = this.getDay();
+			this.setDate(this.getDate() + (dow - thisDow));
 		}
 	}
 };
 
+// store unit list
+var timeUnits = Object.keys(methodsForTimeUnit);
 
-var timeUnits = [];
-for (var i in methodsForTimeUnit) {
-	timeUnits.push(i);
+// define additional methods
+for (var i in timeUnits) {
+	var unit = timeUnits[i];
+	methodsForTimeUnit[unit].add = (function() {
+		var u = ''+unit; // convert closure to local value
+		return function (amount) {
+			methodsForTimeUnit[u].set.call(this, methodsForTimeUnit[u].get.call(this) + amount);
+		};
+	})();
+	methodsForTimeUnit[unit].plusOne = (function() {
+		var u = ''+unit; // convert closure to local value
+		return function () {
+			methodsForTimeUnit[u].add.call(this, 1);
+		};
+	})();
+	methodsForTimeUnit[unit].minusOne = (function() {
+		var u = ''+unit; // convert closure to local value
+		return function () {
+			methodsForTimeUnit[u].add.call(this, -1);
+		};
+	})();
 }
-console.log(timeUnits);
 
-// constructor
+// CrontabTimer constructor
 var CrontabTimer = function () {
 	var that = this;
 	// default value
@@ -77,50 +122,56 @@ var CrontabTimer = function () {
 	});
 };
 
+// CrontabTimer methods
 CrontabTimer.prototype = (function () {
-	// private
-	var checkForUnit = function (date, unit) {
+	// private methods
+	function checkForUnit(date, unit) {
 		// argument check
 		if (date.constructor !== Date) {
 			throw new Error('Date object not given.');
 		}
-		//console.log(methodsForTimeUnit);return;
 		var value = methodsForTimeUnit[unit].get.call(date);
-		console.log('unit: '+unit+', value: '+value);
 		// check minute
 		return -1 !== this.time[timeUnits[0]].findIndex(function (e) {
-			if (e == '*' || value == parseInt(e)) {
+			if (e == '*' || (!isNaN(e) && value == parseInt(e))) {
 				return true;
 			} else {
-				return false;
+				var matchArray = e.match(/\*\/(\d+)/);
+				if (matchArray) {
+					var term = matchArray[1];
+					if (value % parseInt(term) == 0) {
+						return true;
+					}
+				}
 			}
+			return false;
 		});
-	};
+	}
 	var checkForAllUnits = function (date) {
 		for (var unit in methodsForTimeUnit) {
 			var res = checkForUnit.call(this, date, unit);
-			console.log('res: '+res);
 			if (res === false)
-				return false;
+				return unit;
 		}
 		return true;
 	};
+	// calculate next run time and store cache
 	var calcNextTime = function () {
-		// 다음 수행시간 계산
+		log('call calcNextTime');
 		var d = new Date();
-		console.log('result:'+checkForAllUnits.call(this, d));
 
 		this.nextTime++;
 	};
+	// calculate previous run time and store cache
 	var calcPrevTime = function () {
 		log('call calcPrevTime');
 		this.prevTime--;
 	};
 	var timeCheck = function (date) {
 	};
-	// public
+	// public methods
 	return {
-		// 다음 수행시간
+		// next run time
 		getNextTime: function () {
 			log('call getNextTime');
 			// 이미 계산된 값이 유효하지 않으면 해당 값을 리턴
@@ -129,6 +180,7 @@ CrontabTimer.prototype = (function () {
 			}
 			return this.nextTime;
 		},
+		// previous run time
 		getPrevTime: function () {
 			log('call getPrevTime');
 			// 이미 계산된 값이 유효하지 않으면 해당 값을 리턴
@@ -141,7 +193,13 @@ CrontabTimer.prototype = (function () {
 			return this.timeString;
 		},
 		test: function () {
-			//console.log(checkForUnit.call(this,new Date(),timeUnits[0]));
+			var dt = new Date();
+			methodsForTimeUnit['minute'].plusOne.call(dt);
+			console.log(dt.toLocaleTimeString());
+			methodsForTimeUnit['minute'].plusOne.call(dt);
+			console.log(dt.toLocaleTimeString());
+			methodsForTimeUnit['minute'].plusOne.call(dt);
+			console.log(dt.toLocaleTimeString());
 		}
 	};
 })();
